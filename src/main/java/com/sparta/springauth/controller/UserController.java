e64fb4c2cf834774dd3f8a320251af39dd309697
leeminju <leemj4090@naver.com>
2023-11-08 17:04:37 +0900

    회원가입 구현

---
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.sparta.springauth.controller;
+
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";//Rest Controller가 아니기 때문에 templates/login.html 파일 열림
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        //@ModelAttribute 생략 가능
+        userService.signup(requestDto);
+        return "redirect:/api/user/login-page";
+    }
+}
